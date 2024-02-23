26b64471fb41874f2c531550915344eeb4455ed4
Waraht Punyarote <waraht.punyarote@lseg.com>
2024-02-23 14:23:23 +0700

    feat: Embed Todos and basically render

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+import todos from '../public/todos.json';
+import { Todo } from './types';
+
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -9,9 +12,11 @@ export default function Home() {
 							-&gt;
 						</span>
 					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Find in-depth information about Next.js features and API.
-					</p>
+					<div className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+						{(todos as Todo[]).map((todo, i) => (
+							<div key={i}>{todo.name}</div>
+						))}
+					</div>
 				</div>
 
 				<div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
---
+++ app/types.ts
@@ -0,0 +1,4 @@
+export type Todo = {
+	type: string;
+	name: string;
+};
